05e2696fcfcda6d6d9061fb0ba1e3a572874f906
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-04-12 21:25:31 +0300

    add footer and liteners

--- modal-window-pugin/index.js
+++ modal-window-pugin/index.js
@@ -1,5 +1,15 @@
 const modal = $.modal({
     title: 'helloModal',
     width: '500px',
-    content: '<p>my content</p>'
+    content: '<p>my content</p>',
+    footerButtons: [
+        {text: 'Ok', type: 'primary', handler(){
+            console.log('Primary btn clicked')
+            modal.close()
+        }},
+        {text: 'Cancel', type: 'danger', handler(){
+            console.log('Dnger btn clicked')
+            modal.close()
+        }}
+    ]
 })
--- modal-window-pugin/plugins/modal.js
+++ modal-window-pugin/plugins/modal.js
@@ -1,4 +1,29 @@
+Element.prototype.appendAfter = function(element) {
+    element.parentNode.insertBefore(this, element.nextSibling)
+}
+
 $.modal = function(options) {
+    function noop() {}
+
+    function _createModalFooter(buttons = []) {
+        if (buttons.length === 0) {
+            return document.createElement('div')
+        }
+        const wrap = document.createElement('div')
+        wrap.classList.add('modal-footer')
+
+        buttons.forEach(btn => {
+            const $btn = document.createElement('button')
+            $btn.textContent = btn.text
+            $btn.classList.add('btn')
+            $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+            $btn.onclick = btn.handler || noop
+
+            wrap.appendChild($btn)
+        })
+
+        return wrap
+    }
     function _createModal(options) {
         options = {
             ...defaultOptions,
@@ -8,22 +33,20 @@ $.modal = function(options) {
         const modal = document.createElement('div')
         modal.classList.add('modal-w')
         modal.insertAdjacentHTML('afterbegin',`
-            <div class="modal-overlay">
+            <div class="modal-overlay" data-close="true">
                 <div class="modal-window" style="{width:${width};}">
                     <div class="modal-header">
                         <span class="modal-title">${title}</span>
-                        ${closable ? '<span class="modal-close">&times;</span>' : ''}
+                        ${closable ? '<span class="modal-close" data-close="true">&times;</span>' : ''}
                     </div>    
-                    <div class="modal-body">
+                    <div class="modal-body" data-content>
                         ${content}
-                    </div>
-                    <div class="modal-footer">
-                        <button>OK</button>
-                        <button>Cancel</button>
                     </div>
                 </div>
             </div>
         `)
+        const footer = _createModalFooter(options.footerButtons)
+        footer.appendAfter(modal.querySelector('[data-content]'))
         document.body.appendChild(modal)
         return modal
     }
@@ -34,13 +57,16 @@ $.modal = function(options) {
         width: '200px',
         content: '<p>Lorem ipsum dolor sit.</p>',
     }
-
     const $modal = _createModal(options)
     const ANIMATION_SPEED = 200
     let closing = false
+    let destroyed = false
 
-    return {
+    const modal = {
         open() {
+            if (destroyed) {
+                return console.log('Modal is destroyed')
+            }
             !closing && $modal.classList.add('open')
         },
         close() {
@@ -52,8 +78,24 @@ $.modal = function(options) {
                 closing = false
             }, ANIMATION_SPEED)
         },
-        destroy() {
-          
+    }
+    
+    const listener = event => {
+        if (event.target.dataset.close) {
+            modal.close()
         }
     }
+
+    $modal.addEventListener('click', listener) 
+
+    return Object.assign(modal, {
+        destroy() {
+           $modal.parentNode.removeChild($modal);
+           $modal.removeEventListener('click', listener)
+           destroyed = true
+        },
+        setContent(html) {
+            $modal.querySelector('[data-content]').innerHTML = html
+        }
+    })
 }
