e5073a6f4977aa69961bf46d689362bc3daa392d
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-04-12 13:52:03 +0300

    add modal window plugin

---
+++ modal-window-pugin/plugins/modal.js
@@ -0,0 +1,46 @@
+$.modal = function(options) {
+    function _createModal(options) {
+        const modal = document.createElement('div')
+        modal.classList.add('modal-w')
+        modal.insertAdjacentHTML('afterbegin',`
+            <div class="modal-overlay">
+                <div class="modal-window">
+                    <div class="modal-header">
+                        <span class="modal-title">ModalTitle</span>
+                        <span class="modal-close">&times;</span>
+                    </div>    
+                    <div class="modal-body">
+                        <p>Lorem ipsum dolor sit.</p>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button>OK</button>
+                        <button>Cancel</button>
+                    </div>
+                </div>
+            </div>
+        `)
+        document.body.appendChild(modal)
+        return modal
+    }
+
+    const $modal = _createModal(options)
+    const ANIMATION_SPEED = 200
+    let closing = false
+
+    return {
+        open() {
+            !closing && $modal.classList.add('open')
+        },
+        close() {
+            closing = true
+            $modal.classList.remove('open')
+            $modal.classList.add('hide')
+            setTimeout(() => {
+                $modal.classList.remove('hide')
+                closing = false
+            }, ANIMATION_SPEED)
+        },
+        destroy() {}
+    }
+}
