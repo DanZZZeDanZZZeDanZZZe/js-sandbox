fc2f2396a18c39cbe8b5a38ac3b9722f77a9f13d
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-06-30 17:29:04 +0300

    added qsort

---
+++ grok_algorithms/quicksort.js
@@ -0,0 +1,14 @@
+function quicksort(arr) {
+  if (arr.length < 2) return arr
+
+  const pivotIndex = Math.floor(arr.length / 2)
+  const pivot = arr[pivotIndex]
+
+  const less = arr.filter((item, index) => item <= pivot && index !== pivotIndex)
+  const greater = arr.filter((item, index) => item > pivot && index !== pivotIndex)
+  
+  return [...quicksort(less), pivot, ...quicksort(greater)]
+}
+
+const arr = [2,4,6,3,5,7,8,7,2]
+console.log('quicksort(arr)', quicksort(arr))
