e84d086533ca17b3e9694c846ee85e6858319da0
Artem Grankin <artem_grankin@epam.com>
2020-07-24 21:12:21 +0300

    feature: connect db

--- GraphQL-tutorial/schema/schema.js
+++ GraphQL-tutorial/schema/schema.js
@@ -1,4 +1,6 @@
 const graphql = require('graphql')
+const Movie = require('../models/movie')
+const Director = require('../models/director')
 
 const { 
   GraphQLObjectType, 
@@ -9,24 +11,6 @@ const {
   GraphQLList
 } = graphql
 
-const movies = [
-	{ id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1' },
-	{ id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2' },
-	{ id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3' },
-  { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4' },
-  { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
-	{ id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
-	{ id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
-	{ id: '7', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
-]
-
-const directors = [
-	{ id: '1', name: 'Quentin Tarantino', age: 55 },
-	{ id: '2', name: 'Michael Radford', age: 72 },
-	{ id: '3', name: 'James McTeigue', age: 51 },
-	{ id: '4', name: 'Guy Ritchie', age: 50 },
-]
-
 const MoveType = new GraphQLObjectType({
   name: 'Move',
   fields: () => ({
@@ -36,7 +20,7 @@ const MoveType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, args) {
-        return directors.find(director => director.id == parent.id)
+        return Director.findById(parent.directorId)
       }
     }
   })
@@ -51,7 +35,7 @@ const DirectorType = new GraphQLObjectType({
     movies: {
       type: new GraphQLList(MoveType),
       resolve(parent, args) {
-        return movies.filter(movie => movie.directorId === parent.id)
+        return Movie.find({ directorId: parent.id })
       }
     }
   }),
@@ -64,26 +48,26 @@ const Query = new GraphQLObjectType({
       type: MoveType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return movies.find(movie => movie.id == args.id)
+        return Movie.findById(args.id)
       }
     },
     director: {
       type: DirectorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return directors.find(director => director.id == args.id)
+        return Director.findById(args.id)
       }
     },
     movies: {
       type: new GraphQLList(MoveType),
       resolve(parent, args) {
-        return movies
+        return Movie.find({})
       }
     },
     directors: {
       type: new GraphQLList(DirectorType),
       resolve(parent, args) {
-        return directors
+        return Director.find({})
       }
     }
   }
--- GraphQL-tutorial/server/app.js
+++ GraphQL-tutorial/server/app.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('../schema/schema')
+const {connect, connection} = require('mongoose')
+const config = require('config');
+const {username, password, dbName} = config.get('dbConfig');
 
 const app = express()
 const PORT = 3005
+
+connect(
+  `mongodb+srv://${username}:${password}@cluster0.xhwza.azure.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  }
+)
+
+connection.on('error', err => console.log(`Connection error: ${err}`))
+connection.once('open', () => console.log(`Connected to DB`))
+
 
 app.use('/graphql', graphqlHTTP({
   schema,
