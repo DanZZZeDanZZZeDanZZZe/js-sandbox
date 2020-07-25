d1a1e9fc5cce0f7541719ba6dc3753834c9d74f9
Artem Grankin <artem_grankin@epam.com>
2020-07-25 14:40:26 +0300

    feature:  change mongoose scheme

--- GraphQL-tutorial/models/movie.js
+++ GraphQL-tutorial/models/movie.js
@@ -3,7 +3,9 @@ const {Schema, model} = require('mongoose')
 const movieSchema = new Schema({
   name: String,
   genre: String,
-  directorId: String
+  directorId: String,
+  rate: Number,
+  watched: Boolean
 })
 
 module.exports = model('Movie', movieSchema)
