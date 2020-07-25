7d3f3c638bd4b5ccffa4b9474a057f77a56d9cda
Artem Grankin <artem_grankin@epam.com>
2020-07-25 13:57:26 +0300

    chore: connect cors

--- GraphQL-tutorial/server/app.js
+++ GraphQL-tutorial/server/app.js
@@ -3,7 +3,9 @@ const {graphqlHTTP} = require('express-graphql')
 const schema = require('../schema/schema')
 const {connect, connection} = require('mongoose')
 const config = require('config');
+const c = require('config');
 const {username, password, dbName} = config.get('dbConfig');
+const cors = require('cors')
 
 const app = express()
 const PORT = 3005
@@ -19,6 +21,7 @@ connect(
 connection.on('error', err => console.log(`Connection error: ${err}`))
 connection.once('open', () => console.log(`Connected to DB`))
 
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
   schema,
