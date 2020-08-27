6615bbf88736273d8482e947ae9df06f1bcd6397
Artem Grankin <artem_grankin@epam.com>
2020-08-27 22:50:03 +0300

    style: divide functions and routes

--- natours/app.js
+++ natours/app.js
@@ -7,8 +7,16 @@ app.use(express.json())
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours
+    }
+  })
+}
 
+const getTour = (req, res) => {
   const id = req.params.id * 1
   const tour = tours.find(el => el.id === id)
 
@@ -25,9 +33,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour
     }
   })
-})
+}
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1
   const newTour = { id: newId, ...req.body }
   tours.push(newTour)
@@ -40,9 +48,9 @@ app.post('/api/v1/tours', (req, res) => {
       }
     })
   })
-})
+}
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -56,9 +64,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour>'
     }
   })
-})
+}
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -70,7 +78,19 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null
   })
-})
+}
+
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour)
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour)
+
 
 const port = process.env.port || 3000
 app.listen(port, () => {
