f3b900260ec784ed3f029be7eb122d40fa03799a
Artem Grankin <artem_grankin@epam.com>
2020-08-28 22:39:48 +0300

    feature: add date middleware

--- natours/app.js
+++ natours/app.js
@@ -5,11 +5,17 @@ const app = express()
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     data: {
       tours
     }
