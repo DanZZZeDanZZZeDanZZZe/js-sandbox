8b606d396c85c30acdb1234a75c0f1498e30e770
Artem Grankin <artem_grankin@epam.com>
2020-08-28 23:20:29 +0300

    fueature: add morgan

--- natours/app.js
+++ natours/app.js
@@ -1,13 +1,15 @@
 const fs = require('fs')
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use(express.json())
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+  req.requestTime = new Date().toISOString()
+  next()
 })
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
