3e6653171ea2d0d67d525923e6560ae9709587b1
Artem Grankin <artem_grankin@epam.com>
2020-09-11 23:41:06 +0300

    feature: create a better file structure

--- natours/app.js
+++ natours/app.js
@@ -1,10 +1,13 @@
-const fs = require('fs')
 const express = require('express')
 const morgan = require('morgan')
+
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
 app.use(morgan('dev'))
+
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -12,146 +15,7 @@ app.use((req, res, next) => {
   next()
 })
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    data: {
-      tours
-    }
-  })
-}
-
-const getTour = (req, res) => {
-  const id = req.params.id * 1
-  const tour = tours.find(el => el.id === id)
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    })
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  })
-}
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = { id: newId, ...req.body }
-  tours.push(newTour)
-
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    })
-  })
-}
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    })
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour>'
-    }
-  })
-}
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    })
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  })
-}
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  })
-}
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  })
-}
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  })
-}
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  })
-}
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  })
-}
-
-const userRouter = express.Router();
-const tourRouter = express.Router();
-
-tourRouter
-  .route('/')
-  .get(getAllTours)
-  .post(createTour)
-
-tourRouter
-  .route('/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour)
-
-userRouter
-  .route('/')
-  .get(getAllUsers)
-  .post(createUser)
-
-userRouter
-  .route('/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser)
-
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-const port = process.env.port || 3000
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`)
-})
+module.exports = app
---
+++ natours/routes/tourRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const tourController = require('./../controllers/tourController')
+const tourRouter = express.Router()
+
+const { 
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour
+} = tourController
+
+tourRouter
+  .route('/')
+  .get(getAllTours)
+  .post(createTour)
+
+tourRouter
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour)
+
+module.exports = tourRouter
